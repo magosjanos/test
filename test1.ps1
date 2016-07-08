8aac9ab300e5791c53277cb72639ba6a9a890f5c
magosjanos <jan@vanderweide.net>
2016-07-09 00:44:11 +0200

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,3 +1,4 @@
 $location = 'West Europe';
 $resourceGroupName = "BlueSeaStar";
 $nsgName = 'BSSNSG0006';
+$void = Add-AzureRMNetworkSecurityRuleConfig -Name "RULE0001" -NetworkSecurityGroup $nsgObject -Access Allow -Description "Famous last words" -DestinationAddressPrefix "10.42.1.4" -DestinationPortRange "80" -Direction Inbound -Priority 1500 -Protocol Tcp -SourceAddressPrefix "10.42.1.4/32" -SourcePortRange "*"
